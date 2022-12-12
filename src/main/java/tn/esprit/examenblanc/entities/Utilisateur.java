1b5f3b49699db756c702e3b2dfdd430f10aa5b4c
ahmedbnammar <ahmed.benammar1@esprit.tn>
2022-12-12 14:40:36 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/examenblanc/entities/Utilisateur.java
@@ -0,0 +1,14 @@
+package tn.esprit.examenblanc.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Utilisateur {
+    @Id
+    int idUser;
+    
+
+
+}
